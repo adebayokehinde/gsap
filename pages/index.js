21e7a65603652b5a58314041fe386a39c2b7acb7
Yeppman <40640334+Yeppman@users.noreply.github.com>
2021-06-08 19:42:33 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
         <link rel="apple-touch-icon" href="/logo192.png" />
-        <meta
-          name="description"
-          content="Mandoe Pay is an online instant automated p2p system for crypto to fiat naira withdrawal"
-        />
-        <meta name="Mandoe Pay" content="website of Excite Enterprise" />
-        <meta name="keywords" content="Cryptocurrency ,Nigeria Fiat Withdraw" />
-        <meta name="address" content="Lagos Nigeria" />
 
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
